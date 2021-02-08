0a5831f784532529c16c2b6a2ea9441dec65c41d
snr131 <53094202+snr131@users.noreply.github.com>
2021-02-08 13:27:23 -0500

    assignment1_part2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,18 @@
 
 class Book:
-    def __init(self, author, title):
-        pass
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
+        print("{title}, written by {author}".format(title=self.title, author=self.author))
+
+b = ""
+book = Book(b,b)
+book1 = Book(title='Of Mice and Men', author='John Steinbeck')
+book1.display()
+book2 = Book(title='To Kill a Mockingbird', author='Harper Lee')
+book2.display()
         
 if __name__ == "__main__":
     pass
