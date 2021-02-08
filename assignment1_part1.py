a5cbe5ee26240de7433071cf633878fdf6cc8eed
snr131 <53094202+snr131@users.noreply.github.com>
2021-02-08 13:13:02 -0500

    assignment1_part1

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,14 +1,22 @@
 
-def listDivide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
-    pass
+def listDivide(numbers,divide=2):
+  even_nums = []
+  for i in range(0,len(numbers)):
+    if numbers[i] % divide == 0:
+      even_nums.append(numbers[i])
+  return len(even_nums)
+
+class ListDivideException(Exception):
+  pass 
 
 def testListDivide():
-    """
-    Test listDivide
-    """
+    try:
+        result =  listDivide([1,2,3,4,5], divide=2)
+        if result == False:
+            print(result)
+            raise ListDivideException
+    except:
+        print("There is a ListDivideException")
     assert listDivide([1,2,3,4,5]) == 2
     assert listDivide([2,4,6,8,10]) == 5
     assert listDivide([30, 54, 63,98, 100], divide=10) == 2
